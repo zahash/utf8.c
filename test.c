84c192dc01f4b5165768fac8320fff1e6ea66d95
zahash <zahash.z@gmail.com>
2024-03-19 18:50:14 +0530

    incorporate 4 byte chars in tests

--- test.c
+++ test.c
@@ -7,69 +7,70 @@
 // english characters are 1 byte each
 // russian  2 bytes each
 // japanese 3 bytes each
+// 🚩 and 😁 is 4 bytes each
 
 void test_validate_utf8_ok() {
-  utf8_validity validity = validate_utf8("Hello Здравствуйте こんにちは");
+  utf8_validity validity = validate_utf8("Hello Здравствуйте こんにちは 🚩😁");
   assert(validity.valid == true);
-  assert(validity.valid_upto == 5 + 1 + 12 * 2 + 1 + 5 * 3);
+  assert(validity.valid_upto == 5 + 1 + 12 * 2 + 1 + 5 * 3 + 1 + 2 * 4);
 }
 
 void test_validate_utf8_err() {
-  utf8_validity validity = validate_utf8("Hello Здравствуйте\xC0\xC0 こんにちは");
+  utf8_validity validity = validate_utf8("Hello Здравствуйте\xC0\xC0 こんにちは 🚩😁");
   assert(validity.valid == false);
   assert(validity.valid_upto == 5 + 1 + 12 * 2);
 }
 
 void test_make_utf8_string_ok() {
-  const char* str = "Hello Здравствуйте こんにちは";
+  const char* str = "Hello Здравствуйте こんにちは 🚩😁";
   utf8_string ustr = make_utf8_string(str);
-  assert(ustr.byte_len == 5 + 1 + 12 * 2 + 1 + 5 * 3);
+  assert(ustr.byte_len == 5 + 1 + 12 * 2 + 1 + 5 * 3 + 1 + 2 * 4);
   assert(strcmp(ustr.str, str) == 0);
 }
 
 void test_make_utf8_string_err() {
-  utf8_string ustr = make_utf8_string("Hello Здравствуйте\xC0\xC0 こんにちは");
+  utf8_string ustr = make_utf8_string("Hello Здравствуйте\xC0\xC0 こんにちは 🚩😁");
   assert(ustr.str == NULL);
   assert(ustr.byte_len == 0);
 }
 
 void test_make_utf8_string_slice_ok() {
-  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_string_slice slice = make_utf8_string_slice(str, 6, 24);
   assert(slice.byte_len == 12 * 2);
   assert(strncmp(slice.str, "Здравствуйте", slice.byte_len) == 0);
 }
 
 void test_make_utf8_string_slice_start_out_of_bounds_ok() {
-  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_string_slice slice = make_utf8_string_slice(str, 1000, 1);
   assert(slice.byte_len == 0);
   assert(strcmp(slice.str, "") == 0);
 }
 
 void test_make_utf8_string_slice_end_out_of_bounds_ok() {
-  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_string_slice slice = make_utf8_string_slice(str, 6, 1000);
-  assert(slice.byte_len == 12 * 2 + 1 + 5 * 3);
-  assert(strncmp(slice.str, "Здравствуйте こんにちは", slice.byte_len) == 0);
+  assert(slice.byte_len == 12 * 2 + 1 + 5 * 3 + 1 + 2 * 4);
+  assert(strncmp(slice.str, "Здравствуйте こんにちは 🚩😁", slice.byte_len) == 0);
 }
 
 void test_make_utf8_string_slice_start_non_boundary_err() {
-  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_string_slice slice = make_utf8_string_slice(str, 7, 3);
   assert(slice.str == NULL);
   assert(slice.byte_len == 0);
 }
 
 void test_make_utf8_string_slice_end_non_boundary_err() {
-  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string str = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_string_slice slice = make_utf8_string_slice(str, 6, 3);
   assert(slice.str == NULL);
   assert(slice.byte_len == 0);
 }
 
 void test_utf8_char_iter() {
-  utf8_string str = make_utf8_string("Hдこ");
+  utf8_string str = make_utf8_string("Hдこ😁");
   utf8_char_iter iter = make_utf8_char_iter(str);
 
   utf8_char ch;
@@ -86,6 +87,10 @@ void test_utf8_char_iter() {
   assert(ch.byte_len == 3);
   assert(strncmp(ch.str, "こ", ch.byte_len) == 0);
 
+  ch = next_utf8_char(&iter);
+  assert(ch.byte_len == 4);
+  assert(strncmp(ch.str, "😁", ch.byte_len) == 0);
+
   // iterator keeps returning { .str = '\0', .byte_len = 0 } when exhausted
   ch = next_utf8_char(&iter);
   assert(ch.byte_len == 0);
@@ -97,39 +102,47 @@ void test_utf8_char_iter() {
 }
 
 void test_is_utf8_char_boundary() {
-  const char* str = "Hдこ";
+  const char* str = "Hдこ😁";
   assert(is_utf8_char_boundary(str)); //    H
+
   assert(is_utf8_char_boundary(++str)); //  д
   assert(!is_utf8_char_boundary(++str));
+
   assert(is_utf8_char_boundary(++str)); //  こ
   assert(!is_utf8_char_boundary(++str));
   assert(!is_utf8_char_boundary(++str));
+
+  assert(is_utf8_char_boundary(++str)); //  😁
+  assert(!is_utf8_char_boundary(++str));
+  assert(!is_utf8_char_boundary(++str));
+  assert(!is_utf8_char_boundary(++str));
+
   assert(strcmp(++str, "") == 0);
 }
 
 void test_nth_utf8_char_valid_index_ok() {
-  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_char ch = nth_utf8_char(ustr, 20);
   assert(ch.byte_len == 3);
   assert(strncmp(ch.str, "ん", ch.byte_len) == 0);
 }
 
 void test_nth_utf8_char_first_index_ok() {
-  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_char ch = nth_utf8_char(ustr, 0);
   assert(ch.byte_len == 1);
   assert(*ch.str == 'H');
 }
 
 void test_nth_utf8_char_last_index_ok() {
-  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは");
-  utf8_char ch = nth_utf8_char(ustr, 23);
-  assert(ch.byte_len == 3);
-  assert(strncmp(ch.str, "は", ch.byte_len) == 0);
+  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
+  utf8_char ch = nth_utf8_char(ustr, 26);
+  assert(ch.byte_len == 4);
+  assert(strncmp(ch.str, "😁", ch.byte_len) == 0);
 }
 
 void test_nth_utf8_char_invalid_index_err() {
-  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは");
+  utf8_string ustr = make_utf8_string("Hello Здравствуйте こんにちは 🚩😁");
   utf8_char ch = nth_utf8_char(ustr, 100);
   assert(ch.str == NULL);
   assert(ch.byte_len == 0);
